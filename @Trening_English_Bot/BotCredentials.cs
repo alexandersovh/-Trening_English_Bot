7d1c0fea03defc643278c97eef46b574fff76210
Alexandr <alexandrsivoh@gmail.com>
2022-02-17 16:25:48 +0300

    0.1.1

--- @Trening_English_Bot/BotCredentials.cs
+++ @Trening_English_Bot/BotCredentials.cs
@@ -6,6 +6,6 @@ namespace Trening_English_Bot
 {
     public static class BotCredentials
     {
-        public static readonly string BotToken = "1795665102:AAFkEi-RwDs29lsCytlE_10raY7rrN8T_t8"; 
+        public static readonly string BotToken = ""; 
     }
 }
